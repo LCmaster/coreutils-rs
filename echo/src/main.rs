cacf2cc305b8eac5d407310831bc983ac40a29cc
Luiz Vicente <luizcv89@gmail.com>
2023-01-07 18:34:11 -0300

    Finished a very simple implementation. Will improve later

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+#[derive(Parser, Default, Debug)]
+#[command(name = "echo")]
+#[command(author = "Luiz Vicente <luizcv89@gmail.com>")]
+#[command(version = "1.0")]
+#[command(about = "Echo the STRING(s) to standard ouput", long_about = None)]
+struct Arguments {
+    ///Do not output a trailing newline
+    #[arg(short = 'n')]
+    disable_newline:bool,
+    
+    output: Vec<String>
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    let output = args.output;
+    let ending = if args.disable_newline { "" } else { "\n" };
+
+    print!("{}{}", output.join(" "), ending);
 }
