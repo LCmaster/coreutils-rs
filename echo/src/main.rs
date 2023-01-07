56a8abe0d01b583a6d14eee0b79d1fb2bc5d34ab
Luiz Vicente <luizcv89@gmail.com>
2023-01-07 18:55:06 -0300

    fixed typo

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -3,20 +3,20 @@ use clap::Parser;
 #[command(name = "echo")]
 #[command(author = "Luiz Vicente <luizcv89@gmail.com>")]
 #[command(version = "1.0")]
-#[command(about = "Echo the STRING(s) to standard ouput", long_about = None)]
+#[command(about = "Echo the TEXT(s) to standard ouput", long_about = None)]
 struct Arguments {
     ///Do not output a trailing newline
     #[arg(short = 'n')]
     disable_newline:bool,
     
-    output: Vec<String>
+    texts: Vec<String>
 
 }
 
 fn main() {
     let args = Arguments::parse();
-    let output = args.output;
+    let output = args.texts.join(" ");
     let ending = if args.disable_newline { "" } else { "\n" };
 
-    print!("{}{}", output.join(" "), ending);
+    print!("{}{}", output, ending);
 }
