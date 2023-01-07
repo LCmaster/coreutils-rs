decfb6b2d1a15e9b5b4bfd5ebbc5679ced5cbb1c
Luiz Vicente <luizcv89@gmail.com>
2023-01-07 20:35:48 -0300

    basic test added

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 #[command(author = "Luiz Vicente <luizcv89@gmail.com>")]
 #[command(version = "1.0")]
 #[command(about = "Echo the TEXT(s) to standard ouput", long_about = None)]
+#[command(arg_required_else_help(true))]
 struct Arguments {
     ///Do not output a trailing newline
     #[arg(short = 'n')]
@@ -19,4 +20,28 @@ fn main() {
     let ending = if args.disable_newline { "" } else { "\n" };
 
     print!("{}{}", output, ending);
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_cmd::{Command, output};
+    use predicates::prelude::*;
+
+    #[test]
+    fn dies_no_arg() {
+        let mut cmd = Command::cargo_bin("echo").unwrap();
+        cmd.assert().failure().stderr(predicate::str::contains("Usage"));
+    }
+
+    #[test]
+    fn print_hello() {
+        let mut cmd = Command::cargo_bin("echo").unwrap();
+        cmd.arg("hello").assert().success().stdout("hello\n");
+    }
+
+    #[test]
+    fn print_hello_no_trailing_newline() {
+        let mut cmd = Command::cargo_bin("echo").unwrap();
+        cmd.args(vec!["-n", "hello"]).assert().success().stdout("hello");
+    }
 }
